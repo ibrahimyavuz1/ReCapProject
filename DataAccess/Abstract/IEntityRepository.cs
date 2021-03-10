4fee68afcd2b390523985ab8912988f14e1c195f
JustDigitalNomad <bamsibeyrek502@gmail.com>
2021-03-11 00:09:28 +0300

    T in IEntityRepository was restricted! (Second Stage)

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         void Add(T entity);
         void Update(T entity);
